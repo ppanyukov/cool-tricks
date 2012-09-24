87ea23877cdb9c52d68553fa637aa4b133cab3ed
Philip Panyukov <ppanyukov@googlemail.com>
2012-09-24 21:12:44 +0100

    Add CanBeNull type

---
+++ GetRidOfNulls/GetRidOfNulls/CanBeNull.SelfTests.cs
@@ -0,0 +1,171 @@
+﻿namespace GetRidOfNulls
+{
+    using NUnit.Framework;
+
+    public static partial class CanBeNull
+    {
+        private static class SelfTests
+        {
+            [TestFixture]
+            public class Ctor
+            {
+                [Test]
+                public void WorksWithReferenceTypes()
+                {
+                    var x = new CanBeNull<string>("hello");
+                }
+
+                [Test]
+                public void AssignsNullValueCorrectly()
+                {
+                    const string input = "hello";
+                    var x = new CanBeNull<string>(input);
+                    Assert.That(x.Value, Is.SameAs(input));
+                }
+
+                [Test]
+                public void AssignsNonNullValueCorrectly()
+                {
+                    const string input = null;
+                    var x = new CanBeNull<string>(input);
+                    Assert.That(x.Value, Is.Null);
+                }
+            }
+
+            [TestFixture]
+            public class Create
+            {
+                [Test]
+                public void WorksWithReferenceTypes()
+                {
+                    var x = NeverNull.Create("hello");
+                }
+
+                [Test]
+                public void AssignsNullValueCorrectly()
+                {
+                    const string input = "hello";
+                    var x = NeverNull.Create(input);
+                    Assert.That(x.Value, Is.SameAs(input));
+                }
+
+                [Test]
+                public void AssignsNonNullValueCorrectly()
+                {
+                    const string input = null;
+                    var x = CanBeNull.Create(input);
+                    Assert.That(x.Value, Is.Null);
+                }
+            }
+
+            [TestFixture]
+            public class ImplicitOperators
+            {
+                [Test]
+                public void ConvertsToCanBeNull()
+                {
+                    string input = "hello";
+                    CanBeNull<string> canBeNull = input;
+                    Assert.That(canBeNull.Value, Is.SameAs(input));
+                }
+
+                [Test]
+                public void ConvertsFromCanBeNull()
+                {
+                    string input = "hello";
+                    CanBeNull<string> canBeNull = CanBeNull.Create(input);
+
+                    string output = canBeNull;
+                    Assert.That(output, Is.SameAs(input));
+                }
+            }
+
+            [TestFixture]
+            public class Equals
+            {
+                [Test]
+                public void DelegatesToEncapsulatedValue()
+                {
+                    var input = new EqualsCapture();
+                    var x = CanBeNull.Create(input).Equals(input);
+                    Assert.That(input.EqualsCallCount, Is.EqualTo(1));
+                }
+
+                [Test]
+                public void StillWorksWithNulls()
+                {
+                    string input = "";
+                    var x = CanBeNull.Create(input).Equals("some other string");
+                }
+            }
+
+            [TestFixture]
+            public class ToString
+            {
+                [Test]
+                public void DelegatesToEncapsulatedValue()
+                {
+                    var input = new EqualsCapture();
+                    var x = CanBeNull.Create(input).ToString();
+                    Assert.That(input.ToStringCallCount, Is.EqualTo(1));
+                }
+
+                [Test]
+                public void ReturnsEmptyStringWhenNull()
+                {
+                    string input = null;
+                    var x = CanBeNull.Create(input).ToString();
+                    Assert.That(x, Is.EqualTo(""));
+                }
+            }
+
+            [TestFixture]
+            public class GetHashcode
+            {
+                [Test]
+                public void DelegatesToEncapsulatedValue()
+                {
+                    var input = new EqualsCapture();
+                    var x = CanBeNull.Create(input).GetHashCode();
+                    Assert.That(input.GetHashCodeCallCount, Is.EqualTo(1));
+                }
+
+                [Test]
+                public void ReturnsZeroWhenValueIsNull()
+                {
+                    string input = null;
+                    var x = CanBeNull.Create(input).GetHashCode();
+                    Assert.That(x, Is.EqualTo(0));
+                }
+            }
+
+            /// <summary>
+            /// To capture calls to delegated Equals method.
+            /// </summary>
+            private sealed class EqualsCapture
+            {
+                public int EqualsCallCount = 0;
+                public int GetHashCodeCallCount = 0;
+                public int ToStringCallCount = 0;
+
+                public override string ToString()
+                {
+                    this.ToStringCallCount += 1;
+                    return base.ToString();
+                }
+
+                public override int GetHashCode()
+                {
+                    this.GetHashCodeCallCount += 1;
+                    return base.GetHashCode();
+                }
+
+                public override bool Equals(object obj)
+                {
+                    this.EqualsCallCount += 1;
+                    return base.Equals(obj);
+                }
+            }
+        }
+    }
+}
---
+++ GetRidOfNulls/GetRidOfNulls/CanBeNull.cs
@@ -0,0 +1,87 @@
+﻿namespace GetRidOfNulls
+{
+    using System;
+
+    public static partial class CanBeNull
+    {
+        public static CanBeNull<T> Create<T>(T value) where T:class 
+        {
+            return new CanBeNull<T>(value);
+        }
+    }
+
+    /// <summary>
+    /// Modelled after <see cref="Nullable{T}"/>, except works on reference types.
+    /// </summary>
+    public struct CanBeNull<T> where T:class 
+    {
+        private readonly T value;
+
+        public CanBeNull(T value)
+        {
+            this.value = value;
+        }
+
+        public T Value
+        {
+            get
+            {
+                return this.value;
+            }
+        }
+
+        public bool HasValue
+        {
+            get
+            {
+                return this.value != null;
+            }
+        }
+
+        public static implicit operator T(CanBeNull<T> other)
+        {
+            return other.Value;
+        }
+
+        public static implicit operator CanBeNull<T>(T value)
+        {
+            return new CanBeNull<T>(value);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if(this.HasValue)
+            {
+                return this.Value.Equals(obj);
+            }
+            else
+            {
+                return Object.Equals(obj, null);
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            if (this.HasValue)
+            {
+                return this.Value.GetHashCode();
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public override string ToString()
+        {
+            if (this.HasValue)
+            {
+                return this.Value.ToString();
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
